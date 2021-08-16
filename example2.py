f2dc796d4c0f0e2c35d64dbf9e9e00bcc952aa8d
Jon Mockbee <mockbee1000@gmail.com>
2021-08-16 19:28:40 -0400

    solved

---
+++ example2.py
@@ -0,0 +1,8 @@
+valuesofnumbers = [23,52,59,37,48]
+sum = 0
+length = 0  
+for value in valuesofnumbers:
+    sum += value
+    length += 1 
+
+print ("total sum: "+ str(sum )+ " -average: " + str(sum/length))
